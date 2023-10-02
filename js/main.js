62e89e57ada1cbe23a1e2266bbfa93258954de01
frolpif <115400244+frolpif@users.noreply.github.com>
2023-10-02 20:35:26 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,6 +17,34 @@
       }
 
   }
+
+  const tabControls = document.querySelector('.tab-controls')
+  tabControls.addEventListener('click', toggleTab)
+
+  function toggleTab(e){
+    const tabControl = e.target.closest('.tab-controls__link')
+    if (!tabControl) return
+    e.preventDefault()
+    if (tabControl.classList.contains('tab-controls__link--active')) return
+
+    const tabContentID = tabControl.getAttribute('href')
+    const tabContent = document.querySelector(tabContentID)
+    const activeControl = document.querySelector('.tab-controls__link--active')
+    const acviveContent = document.querySelector('.tab-content--show')
+
+    if(activeControl){
+      activeControl.classList.remove('tab-controls__link--active')
+    }
+
+    if(activeControl){
+      acviveContent.classList.remove('tab-content--show')
+    }
+
+    tabContent.classList.add('tab-content--show')
+
+
+    tabControl.classList.add('tab-controls__link--active')
+  }
 }
 )()
 
@@ -36,7 +64,6 @@ btn.addEventListener('click', openModal)
 modal.addEventListener('click', event =>{
  event.preventDefault()
   const target = event.target
-  console.log(target)
   if (target && target.classList.contains('modal') || target.closest('.modal__cancel')){
     closeModal()
   }
