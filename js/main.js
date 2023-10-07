ed80cd0bc3d29f2ce0cd1e96a0b5dfb61e9d699e
frolpif <115400244+frolpif@users.noreply.github.com>
2023-10-07 20:11:02 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -45,8 +45,40 @@
 
     tabControl.classList.add('tab-controls__link--active')
   }
-}
-)()
+
+  //аккордеон
+  const accordionLists = document.querySelectorAll('.accordion-list');
+
+  accordionLists.forEach(el => {
+
+      el.addEventListener('click', (e) => {
+
+          const accordionList = e.currentTarget
+          const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+          const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+
+          const accordionControl = e.target.closest('.accordion-list__control');
+          if (!accordionControl) return
+          const accordionItem = accordionControl.parentElement;
+          const accordionContent = accordionControl.nextElementSibling;
+
+          if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+              accordionOpenedItem.classList.remove('accordion-list__item--opened');
+              accordionOpenedContent.style.maxHeight = null;
+          }
+          accordionItem.classList.toggle('accordion-list__item--opened');
+
+          if (accordionItem.classList.contains('accordion-list__item--opened')) {
+              accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+          } else {
+              accordionContent.style.maxHeight = null;
+          }
+
+      });
+
+  });
+
+})()
 
 const btn = document.querySelector('.about__img-button')
 const modal = document.querySelector('.body')
